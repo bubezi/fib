e597b33f7eb7547afd32ac5de7a525c2237be3b2
blackiriku <blackiriku12@gmail.com>
2023-04-24 09:47:36 +0300

    Update

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
-def fib(n):
-    if n<=1:
-        return n
-    else:
-        return fib(n-1) + fib(n-2)
-   
-    
 def fib_numbers(n):
+    """Returns a list of fibonaci numbers"""
+    def fib(x):
+        if x<=1:
+            return x
+        else:
+            return fib(x-1) + fib(x-2)
+    
     numbers = []
     for i in range(n):
         numbers.append(fib(i))
